945a6f6458bdf780f17964b60a221af8c415f8c8
deepak <dpk9111996@gmail.com>
2021-03-31 15:39:34 +0530

    fixes

--- sail_bpm/sail_bpm/custom_scripts/jinja_file/jinja_file.py
+++ sail_bpm/sail_bpm/custom_scripts/jinja_file/jinja_file.py
@@ -10,11 +10,8 @@ from datetime import datetime, timedelta
 
 @frappe.whitelist()
 def get_delivery_note_data(doc):
-	#'{:.3f}'.format(round(2606.89579999999, 3))
-	#print("#########@@@@@@@@@@@@@@@@@@")
 	data = frappe.db.sql("""select item_code,qty,number_of_pieces,wt_range from `tabDelivery Note Item` where parent ="%s" order by item_code"""%(doc.name))
 	data_aggregate = frappe.db.sql("""select item_code,sum(qty) as sumgrosswt,sum(number_of_pieces) as sum_qty from `tabDelivery Note Item` where parent ="%s" group by item_code"""%(doc.name))
-	#print("kkkkkkkkkkkkkkkkk",data_aggregate)
 	table_height=0
 	sendata={}
 	total_bundle=0
@@ -23,7 +20,6 @@ def get_delivery_note_data(doc):
 	last_data_aggregate_count =0
 	for i in data_aggregate:
 		last_data_aggregate_count += 1
-		#header = """ <table style ="width :350px"><tbody><tr class="cls_003" style="border: 1px solid black;"><th colspan ="4"  style="text-align:center" ><strong >%s</strong></th></tr><tr ><td><strong>NO</strong></td><td><strong>Wt Range</strong></td><td><strong>Qty</strong></td><td><strong>Gross Wt</strong></td></tr>"""%(i[0])
 		header = add_data(""" <table style ='width :200px'><tbody><tr class='cls_003' style='border: 1px solid black;'><th colspan ='4'  style='text-align:center' ><strong >%s</strong></th></tr>"""%(i[0]),table_height)
 		table_height += 1
 		header += add_data("""<tr ><td><strong>NO</strong></td><td><strong>Wt Range</strong></td><td><strong>Qty</strong></td><td><strong>Gross Wt</strong></td></tr>""",table_height)
@@ -34,7 +30,6 @@ def get_delivery_note_data(doc):
 				header += add_data("""<tr><td>%s</td><td  align="right">%s</td><td align="right">%s</td><td align="right">%s</td></tr>"""%(count,'{:.3f}'.format(round(j[3], 3)),j[2],'{:.3f}'.format(round(j[1], 3))),table_height)
 				table_height += 1
 				count+=1
-		#header += """<tr><td><strong>%s</strong></td><td><strong>%s</strong></td><td><strong>%s</strong></td><td><strong>%s</strong></td></tr></tbody></table><p   align="justify">&nbsp;</p>"""%(count,"Bun",i[2],i[1])
 		header += add_data("""<tr><td><strong>%s</strong></td><td align="left"><strong>%s</strong></td><td align="right"><strong>%s</strong></td><td align="right"><strong>%s</strong></td></tr></tbody></table>"""%(count-1,"Bun",'{:.0f}'.format(round(i[2], 0)),'{:.3f}'.format(round(i[1], 3))),table_height)
 		table_height += 1
 		if last_data_aggregate_count == len(data_aggregate):
