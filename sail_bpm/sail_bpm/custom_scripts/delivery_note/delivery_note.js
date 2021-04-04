af81c91350c985f198d9419a2a81d5050edf28c7
Sangram Patil <sangram.p@indictranstech.com>
2021-04-04 17:49:24 +0530

    [Delivery Note Script Modifications] calculation

--- sail_bpm/sail_bpm/custom_scripts/delivery_note/delivery_note.js
+++ sail_bpm/sail_bpm/custom_scripts/delivery_note/delivery_note.js
@@ -1,4 +1,4 @@
-frappe.ui.form.on("Delivery Note ", {
+/*frappe.ui.form.on("Delivery Note ", {
 	
 	refresh: function(frm){
 		
@@ -69,4 +69,28 @@ frappe.ui.form.on("Delivery Note Item", {
    console.log(item.qty,"length")
     frm.refresh_field('Delivery Note Item');
   }
+})
+*/
+
+frappe.ui.form.on("Delivery Note", {
+    calculate_wt_range: function(frm, row) {
+        if(row.qty > 0 && row.number_of_pieces > 0) {
+            row.wt_range = flt(row.qty / row.number_of_pieces)
+        }
+        else {
+            row.wt_range = 0
+        }
+        refresh_field("wt_range", row.name, "items");
+    }
+})
+
+frappe.ui.form.on("Delivery Note Item", {
+    number_of_pieces: function(frm, cdt, cdn) {
+        var row = locals[cdt][cdn];
+        frm.events.calculate_wt_range(frm, row);
+    },
+    qty: function(frm, cdt, cdn) {
+        var row = locals[cdt][cdn];
+        frm.events.calculate_wt_range(frm, row);
+    }
 })
