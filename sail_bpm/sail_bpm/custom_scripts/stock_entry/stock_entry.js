eaa645dfe708553bb33c9c1644e21b052a0ae576
Sangram Patil <sangram.p@indictranstech.com>
2021-06-17 08:40:56 +0530

    [Row wise barcode print provision]

--- sail_bpm/sail_bpm/custom_scripts/stock_entry/stock_entry.js
+++ sail_bpm/sail_bpm/custom_scripts/stock_entry/stock_entry.js
@@ -7,6 +7,38 @@ frappe.ui.form.on("Stock Entry", {
             row.wt_range = 0
         }
         refresh_field("wt_range", row.name, "items");
+    },
+
+    refresh: function(frm) {
+        // find item grid row and append print btn/icon
+        var item_wrapper = frm.fields_dict.items.grid.wrapper
+        var item_rows = item_wrapper.find('.grid-row').slice(1)
+        $.each(item_rows, function(idx, r) {
+            if(!$(r).find('.printb').length) {
+                $(r).append('\
+                    <span class="fa fa-print printb"></span> \
+                ')
+            }
+        })
+        //css
+        $('.printb').css({
+            "cursor": "pointer",
+            "margin-left": -3,
+            "font-size": "15px"
+        })
+        //capture row idx & open print preview
+        $('.printb').click(function(e) {
+            var idx = e.currentTarget.closest(".grid-row").getAttribute("data-idx") || 0
+            window.open(frappe.urllib.get_full_url("/printview?"
+                + "doctype=" + encodeURIComponent(frm.doc.doctype)
+                + "&name=" + encodeURIComponent(frm.doc.name)
+                + "&trigger_print=1"
+                + "&format=Barcode Print"
+                + "&idx="+ idx
+                + "&no_letterhead=0"
+                + "&_lang=en"
+           ));
+        })
     }
 })
 
